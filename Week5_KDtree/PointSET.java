d64ad51926a96ca8043213a1e942759451d69dca
Ce Dong <cedong@usc.edu>
2018-01-23 22:42:47 -0800

    Update PointSET.java

--- Week5_KDtree/PointSET.java
+++ Week5_KDtree/PointSET.java
@@ -11,33 +11,36 @@ public class PointSET {
     
     public PointSET() {
         this.dataSet = new SET<Point2D>();
-    } // construct an empty set of points 
+    } // construct an empty set of points
     
     public boolean isEmpty() {
         return dataSet.isEmpty();
-    } // is the set empty? 
+    } // is the set empty?
     
     public int size() {
         return dataSet.size();
-    } // number of points in the set 
+    } // number of points in the set
     
     public void insert(Point2D p) {
+        if (p == null) throw new  java.lang.IllegalArgumentException();
         if (!dataSet.contains(p)) {
             dataSet.add(p);
-        }    
+        }
     } // add the point to the set (if it is not already in the set)
     
     public boolean contains(Point2D p) {
+        if (p == null) throw new  java.lang.IllegalArgumentException();
         return dataSet.contains(p);
-    } // does the set contain point p? 
+    } // does the set contain point p?
     
     public void draw() {
         for (Point2D a : dataSet) {
             a.draw();
         }
-    } // draw all points to standard draw 
+    } // draw all points to standard draw
     
     public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null) throw new  java.lang.IllegalArgumentException();
         Queue<Point2D> result = new LinkedList<Point2D>();
         for (Point2D a : dataSet) {
             if (rect.contains(a)) {
@@ -45,26 +48,27 @@ public class PointSET {
             }
         }
         return result;
-    }  // all points that are inside the rectangle (or on the boundary) 
+    }  // all points that are inside the rectangle (or on the boundary)
     
     public Point2D nearest(Point2D p) {
+        if (p == null) throw new  java.lang.IllegalArgumentException();
         double dist = 0;
         Point2D neighbor = null;
         int i = 0;
         for (Point2D a : dataSet) {
             if (i == 0) {
-                dist = p.distanceTo(a);
+                dist = p.distanceSquaredTo(a);
                 neighbor = a;
             }
-            double temp = p.distanceTo(a);
+            double temp = p.distanceSquaredTo(a);
             if (temp < dist) {
                 dist = temp;
                 neighbor = a;
-        }
+            }
             i++;
-      }
+        }
         return neighbor;
-    } // a nearest neighbor in the set to point p; null if the set is empty 
-
-    // public static void main(String[] args) // unit testing of the methods (optional) 
+    } // a nearest neighbor in the set to point p; null if the set is empty
+    
+    // public static void main(String[] args) // unit testing of the methods (optional)
 }
